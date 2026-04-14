5ac13d4cf9c501308ca2463bdacc4f2b628d3b4c
lets5steady <lets.05.steady@gmail.com>
2026-04-14 19:07:59 +0900

    015_20260414_メニュー外をクリックするとドロワーメニューを閉じる処理を追加

--- js/easyMenu.js
+++ js/easyMenu.js
@@ -11,3 +11,11 @@ toggleButton.addEventListener('click', function () {
     drawerMenu.classList.toggle('open');
     toggleButton.classList.toggle('close');
 });
+
+// メニュー外をクリックした際にドロワーを閉じる処理
+document.addEventListener('click', function (e) {
+    if (!drawerMenu.contains(e.target) && !toggleButton.contains(e.target)) {
+        drawerMenu.classList.remove('open');
+        toggleButton.classList.remove('close');
+    }
+});
